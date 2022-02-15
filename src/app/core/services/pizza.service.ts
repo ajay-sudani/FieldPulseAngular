d4cf81edf9024652cdeecea2ebec2765c5fa9c70
Ajay <ajaysudani07@gmail.com>
2022-02-15 22:55:21 +0530

    fix : typescript typing

--- src/app/core/services/pizza.service.ts
+++ src/app/core/services/pizza.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IPizzeria, IPizza } from "../models";
 
 @Injectable()
 export class PizzaService {
   constructor(private http: HttpClient) {}
 
-  public getPizzeriasJSON(): Observable<any> {
-    return this.http.get('./assets/json/pizzerias.json');
+  public getPizzeriasJSON(): Observable<IPizzeria[]> {
+    return this.http.get<IPizzeria[]>("./assets/json/pizzerias.json");
   }
 
-  public getPizzaJSON(): Observable<any> {
-    return this.http.get('./assets/json/pizza.json');
+  public getPizzaJSON(): Observable<IPizza[]> {
+    return this.http.get<IPizza[]>("./assets/json/pizza.json");
   }
 
-  public getUsTaxRate(): Observable<any> {
-    return this.http.get('./assets/json/settings.json');
+  public getUsTaxRate(): Observable<{ us_tax_rate: number }> {
+    return this.http.get<{ us_tax_rate: number }>(
+      "./assets/json/settings.json"
+    );
   }
 }
